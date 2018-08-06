c0866a66e1c7a48196cc3bddaf3acaf41fd17820
Omar Albeik <omar.albeik@mobilion.com.tr>
2018-08-06 14:49:01 +0300

    fix: Fixes #50

--- Sources/Classes/PCTextField.swift
+++ Sources/Classes/PCTextField.swift
@@ -151,9 +151,11 @@ public extension Crustable where Self: UITextField {
 
 	/// Text field text as email address _(if applicable)_.
 	public var emailAddress: String? {
-		let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-		guard trimmedText.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil else { return nil }
-		return trimmedText
+		guard let possibleEmail = text else { return nil }
+		let regex = "^(?:[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\p{L}0-9](?:[a-z0-9-]*[\\p{L}0-9])?\\.)+[\\p{L}0-9](?:[\\p{L}0-9-]*[\\p{L}0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$"
+
+		guard possibleEmail.range(of: regex, options: .regularExpression, range: nil, locale: nil) != nil else { return nil }
+		return possibleEmail
 	}
 
 	/// Check if text field's text is a valid email address.
